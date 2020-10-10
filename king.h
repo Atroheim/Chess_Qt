6b953a7cd9ab7568772a0594b2f58da87afa687e
Atroheim <70745682+Atroheim@users.noreply.github.com>
2020-10-10 11:07:31 +0800

    Create king.h

---
+++ king.h
@@ -0,0 +1,11 @@
+#ifndef KING_H
+#define KING_H
+#include "chesspiece.h"
+class King:public ChessPiece
+{
+public:
+    King(QString team,QGraphicsItem *parent = 0);
+    void setImage();
+    void moves();
+};
+#endif // KING_H
